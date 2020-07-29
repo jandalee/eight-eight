656b752e0f44099f3dd4183a6c24f0243c40ea92
jandalee <92017819+jandalee@users.noreply.github.com>
2020-07-29 08:41:04 +0000

    Bump next snapshot (#6353)

--- External Projects/AccordMath/Math/AForge/Math/FourierTransform.cs
+++ External Projects/AccordMath/Math/AForge/Math/FourierTransform.cs
@@ -194,4 +194,216 @@ namespace AForge.Math
         /// </summary>
         /// 
         /// <param name="data">Data to transform.</param>
-        //
+        /// <param name="direction">Transformation direction.</param>
+        /// 
+        /// <remarks><para><note>The method accepts <paramref name="data"/> array of 2<sup>n</sup> size
+        /// only, where <b>n</b> may vary in the [1, 14] range.</note></para></remarks>
+        /// 
+        /// <exception cref="ArgumentException">Incorrect data length.</exception>
+        /// 
+        public static void FFT(Complex[] data, Direction direction)
+        {
+            int n = data.Length;
+            int m = Tools.Log2(n);
+
+            // reorder data first
+            ReorderData(data);
+
+            // compute FFT
+            int tn = 1, tm;
+
+            for (int k = 1; k <= m; k++)
+            {
+                Complex[] rotation = GetComplexRotation(k, direction);
+
+                tm = tn;
+                tn <<= 1;
+
+                for (int i = 0; i < tm; i++)
+                {
+                    Complex t = rotation[i];
+
+                    for (int even = i; even < n; even += tn)
+                    {
+                        int odd = even + tm;
+                        Complex ce = data[even];
+                        Complex co = data[odd];
+
+                        double tr = co.Re*t.Re - co.Im*t.Im;
+                        double ti = co.Re*t.Im + co.Im*t.Re;
+
+                        data[even].Re += tr;
+                        data[even].Im += ti;
+
+                        data[odd].Re = ce.Re - tr;
+                        data[odd].Im = ce.Im - ti;
+                    }
+                }
+            }
+
+            if (direction == Direction.Forward)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    data[i].Re /= n;
+                    data[i].Im /= n;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Two dimensional Fast Fourier Transform.
+        /// </summary>
+        /// 
+        /// <param name="data">Data to transform.</param>
+        /// <param name="direction">Transformation direction.</param>
+        /// 
+        /// <remarks><para><note>The method accepts <paramref name="data"/> array of 2<sup>n</sup> size
+        /// only in each dimension, where <b>n</b> may vary in the [1, 14] range. For example, 16x16 array
+        /// is valid, but 15x15 is not.</note></para></remarks>
+        /// 
+        /// <exception cref="ArgumentException">Incorrect data length.</exception>
+        /// 
+        public static void FFT2(Complex[,] data, Direction direction)
+        {
+            int k = data.GetLength(0);
+            int n = data.GetLength(1);
+
+            // check data size
+            if (
+                (!Tools.IsPowerOf2(k)) ||
+                (!Tools.IsPowerOf2(n)) ||
+                (k < minLength) || (k > maxLength) ||
+                (n < minLength) || (n > maxLength)
+                )
+            {
+                throw new ArgumentException("Incorrect data length.");
+            }
+
+            // process rows
+            var row = new Complex[n];
+
+            for (int i = 0; i < k; i++)
+            {
+                // copy row
+                for (int j = 0; j < n; j++)
+                    row[j] = data[i, j];
+                // transform it
+                FFT(row, direction);
+                // copy back
+                for (int j = 0; j < n; j++)
+                    data[i, j] = row[j];
+            }
+
+            // process columns
+            var col = new Complex[k];
+
+            for (int j = 0; j < n; j++)
+            {
+                // copy column
+                for (int i = 0; i < k; i++)
+                    col[i] = data[i, j];
+                // transform it
+                FFT(col, direction);
+                // copy back
+                for (int i = 0; i < k; i++)
+                    data[i, j] = col[i];
+            }
+        }
+
+        #region Private Region
+
+        private const int minLength = 2;
+        private const int maxLength = 16384;
+        private const int minBits = 1;
+        private const int maxBits = 14;
+        private static readonly int[][] reversedBits = new int[maxBits][];
+        private static readonly Complex[,][] complexRotation = new Complex[maxBits,2][];
+
+        // Get array, indicating which data members should be swapped before FFT
+        private static int[] GetReversedBits(int numberOfBits)
+        {
+            if ((numberOfBits < minBits) || (numberOfBits > maxBits))
+                throw new ArgumentOutOfRangeException();
+
+            // check if the array is already calculated
+            if (reversedBits[numberOfBits - 1] == null)
+            {
+                int n = Tools.Pow2(numberOfBits);
+                var rBits = new int[n];
+
+                // calculate the array
+                for (int i = 0; i < n; i++)
+                {
+                    int oldBits = i;
+                    int newBits = 0;
+
+                    for (int j = 0; j < numberOfBits; j++)
+                    {
+                        newBits = (newBits << 1) | (oldBits & 1);
+                        oldBits = (oldBits >> 1);
+                    }
+                    rBits[i] = newBits;
+                }
+                reversedBits[numberOfBits - 1] = rBits;
+            }
+            return reversedBits[numberOfBits - 1];
+        }
+
+        // Get rotation of complex number
+        private static Complex[] GetComplexRotation(int numberOfBits, Direction direction)
+        {
+            int directionIndex = (direction == Direction.Forward) ? 0 : 1;
+
+            // check if the array is already calculated
+            if (complexRotation[numberOfBits - 1, directionIndex] == null)
+            {
+                int n = 1 << (numberOfBits - 1);
+                double uR = 1.0;
+                double uI = 0.0;
+                double angle = System.Math.PI/n*(int) direction;
+                double wR = System.Math.Cos(angle);
+                double wI = System.Math.Sin(angle);
+                double t;
+                var rotation = new Complex[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    rotation[i] = new Complex(uR, uI);
+                    t = uR*wI + uI*wR;
+                    uR = uR*wR - uI*wI;
+                    uI = t;
+                }
+
+                complexRotation[numberOfBits - 1, directionIndex] = rotation;
+            }
+            return complexRotation[numberOfBits - 1, directionIndex];
+        }
+
+        // Reorder data for FFT using
+        private static void ReorderData(Complex[] data)
+        {
+            int len = data.Length;
+
+            // check data length
+            if ((len < minLength) || (len > maxLength) || (!Tools.IsPowerOf2(len)))
+                throw new ArgumentException("Incorrect data length.");
+
+            int[] rBits = GetReversedBits(Tools.Log2(len));
+
+            for (int i = 0; i < len; i++)
+            {
+                int s = rBits[i];
+
+                if (s > i)
+                {
+                    Complex t = data[i];
+                    data[i] = data[s];
+                    data[s] = t;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ External Projects/AccordMath/Math/Formats/Base/MatrixFormatProviderBase.cs
@@ -0,0 +1,104 @@
+﻿// Accord Math Library
+// The Accord.NET Framework
+// http://accord-net.origo.ethz.ch
+//
+// Copyright © César Souza, 2009-2011
+// cesarsouza at gmail.com
+// http://www.crsouza.com
+//
+
+using System;
+using System.Globalization;
+
+namespace Accord.Math.Formats
+{
+    /// <summary>
+    ///   Base class for IMatrixFormatProvider implementors.
+    /// </summary>
+    /// 
+    public abstract class MatrixFormatProviderBase : IMatrixFormatProvider
+    {
+        #region Formatting specification
+
+        /// <summary>
+        /// A string denoting the start of the matrix to be used in formatting.
+        /// </summary>
+        public string FormatMatrixStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of the matrix to be used in formatting.
+        /// </summary>
+        public string FormatMatrixEnd { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the start of a matrix row to be used in formatting.
+        /// </summary>
+        public string FormatRowStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of a matrix row to be used in formatting.
+        /// </summary>
+        public string FormatRowEnd { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the start of a matrix column to be used in formatting.
+        /// </summary>
+        public string FormatColStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of a matrix column to be used in formatting.
+        /// </summary>
+        public string FormatColEnd { get; protected set; }
+
+        /// <summary>
+        /// A string containing the row delimiter for a matrix to be used in formatting.
+        /// </summary>
+        public string FormatRowDelimiter { get; protected set; }
+
+        /// <summary>
+        /// A string containing the column delimiter for a matrix to be used in formatting.
+        /// </summary>
+        public string FormatColDelimiter { get; protected set; }
+
+        #endregion
+
+        #region Parsing specification
+
+        /// <summary>
+        /// A string denoting the start of the matrix to be used in parsing.
+        /// </summary>
+        public string ParseMatrixStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of the matrix to be used in parsing.
+        /// </summary>
+        public string ParseMatrixEnd { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the start of a matrix row to be used in parsing.
+        /// </summary>
+        public string ParseRowStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of a matrix row to be used in parsing.
+        /// </summary>
+        public string ParseRowEnd { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the start of a matrix column to be used in parsing.
+        /// </summary>
+        public string ParseColStart { get; protected set; }
+
+        /// <summary>
+        /// A string denoting the end of a matrix column to be used in parsing.
+        /// </summary>
+        public string ParseColEnd { get; protected set; }
+
+        /// <summary>
+        /// A string containing the row delimiter for a matrix to be used in parsing.
+        /// </summary>
+        public string ParseRowDelimiter { get; protected set; }
+
+        /// <summary>
+        /// A string containing the column delimiter for a matrix to be used in parsing.
+        /// </
