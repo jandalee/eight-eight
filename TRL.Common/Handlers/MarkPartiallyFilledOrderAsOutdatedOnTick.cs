5ad5a6488775f253d6b5f46c5806178bd44bafd4
jandalee <92017819+jandalee@users.noreply.github.com>
2020-11-01 21:04:53 +0000

    Updated project name

--- TRL.Common/Handlers/MarkPartiallyFilledOrderAsOutdatedOnTick.cs
+++ TRL.Common/Handlers/MarkPartiallyFilledOrderAsOutdatedOnTick.cs
@@ -31,4 +31,55 @@ namespace TRL.Common.Handlers
         }
 
         public override void OnItemAdded(Tick item)
-        
+        {
+            if (strategyHeader.Symbol != item.Symbol)
+                return;
+
+            double amount = this.tradingData.GetAmount(this.strategyHeader);
+
+            if (amount == 0)
+                return;
+
+            IEnumerable<Order> filledPartially = this.tradingData.GetFilledPartially(this.strategyHeader, OrderType.Limit);
+
+            if (filledPartially == null)
+                return;
+
+            if (filledPartially.Count() == 0)
+                return;
+
+            Order o = filledPartially.First();
+
+            if (o.TradeAction == TradeAction.Buy && amount < 0)
+                return;
+
+            if (o.TradeAction == TradeAction.Sell && amount > 0)
+                return;
+
+            if (CancelOrderRequestExists(o.Id))
+            {
+                if(!CancelOrderRequestIsOutdated(o.Id))
+                    return;
+            }
+
+            if (o.DateTime.AddSeconds(this.outdateSeconds) > item.DateTime)
+                return;
+
+            string descr = String.Format("Отменить заявку {0}, потому что она не исполнилась полностью в течение {1} секунд", o.ToString(), this.outdateSeconds);
+            this.logger.Log(String.Format("{0:dd/MM/yyyy H:mm:ss.fff}, {1}, {2}", DateTime.Now, this.GetType().Name, descr));
+            this.tradingData.Get<ICollection<OrderCancellationRequest>>().Add(new OrderCancellationRequest(o, descr));
+        }
+
+        private bool CancelOrderRequestExists(int orderId)
+        {
+            return this.tradingData.Get<IEnumerable<OrderCancellationRequest>>().Any(o => o.OrderId == orderId);
+        }
+
+        private bool CancelOrderRequestIsOutdated(int orderId)
+        {
+            return this.tradingData.Get<IEnumerable<OrderCancellationRequest>>().Any(o => o.OrderId == orderId
+                && o.DateTime < o.DateTime.AddSeconds(this.outdateSeconds * 3));
+        }
+
+    }
+}
---
+++ TRL.Common/Handlers/SendItemOnOrder.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TRL.Common.Events;
+using TRL.Common.Data;
+using TRL.Common.Extensions.Data;
+using TRL.Common.Models;
+using TRL.Common.Collections;
+using TRL.Logging;
+//using TRL.Common.Extensions;
+
+namespace TRL.Common.Handlers
+{
+    public class SendItemOnOrder:IObserver
+    {
+        //private IDataContext tradingData;
+        private ObservableQueue<Order> orderQueue;
+        //private IOrderManager manager;
+        //private ILogger logger;
+
+        //public OrderQueueProcessor(IOrderManager manager, IDataContext tradingData, ObservableQueue<Order> orderQueue, ILogger logger)
+        //{
+        //    this.manager = manager;
+        //    this.tradingData = tradingData;
+        //    this.orderQueue = orderQueue;
+        //    this.logger = logger;
+
+        //    this.orderQueue.RegisterObserver(this);
+        //}
+
+        /// <summary>
+        /// сторонний обработчик
+        /// </summary>
+        private ItemAddedNotification<Order> OrderHandler;
+
+        public SendItemOnOrder(ObservableQueue<Order> orderQueue)
+        {
+            this.orderQueue = orderQueue;
+            this.orderQueue.RegisterObserver(this);
+        }
+
+
+        public void Update()
+        {
+            Order order = this.orderQueue.Dequeue();
+            OrderHand
