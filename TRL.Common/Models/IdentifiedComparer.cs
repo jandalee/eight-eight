20a3a8fc8f8991f341d568713f78b6e326dc83c5
jandalee <92017819+jandalee@users.noreply.github.com>
2020-11-16 19:50:39 +0000

    Added example config file

--- TRL.Common/Models/IdentifiedComparer.cs
+++ TRL.Common/Models/IdentifiedComparer.cs
@@ -5,4 +5,19 @@ using System.Text;
 
 namespace TRL.Common.Models
 {
-    public class IdentifiedComparer : Equality
+    public class IdentifiedComparer : EqualityComparer<IIdentified>
+    {
+        public override bool Equals(IIdentified x, IIdentified y)
+        {
+            if (x.Id == y.Id)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode(IIdentified obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
