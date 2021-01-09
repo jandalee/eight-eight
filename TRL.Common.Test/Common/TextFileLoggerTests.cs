ce1ddd51bb91617e65feb67b4d4b700d8ada7749
jandalee <92017819+jandalee@users.noreply.github.com>
2021-01-09 10:51:52 +0000

    add tutorial

--- TRL.Common.Test/Common/TextFileLoggerTests.cs
+++ TRL.Common.Test/Common/TextFileLoggerTests.cs
@@ -32,4 +32,41 @@ namespace TRL.Common.Test
         [TestMethod]
         public void Common_LogMessage()
         {
-            string message = String.Concat(DateTime.Now.ToLocalTime(), ";Тестовое сообщение в ж
+            string message = String.Concat(DateTime.Now.ToLocalTime(), ";Тестовое сообщение в журнал");
+            this.logger.Log(message);
+
+            Assert.IsTrue(File.Exists("default-0.log"));
+
+            StreamReader streamReader = new StreamReader(new FileStream("default-0.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+            Assert.AreEqual(message, streamReader.ReadLine());
+                
+            streamReader.Close();
+            streamReader.Dispose();
+        }
+
+        [TestMethod]
+        public void Common_Create_New_File_On_Size_Limit_Exceed()
+        {
+            TextFileLogger logger = new TextFileLogger("log", 10000);
+
+            for (int i = 0; i < 200; i++)
+                logger.Log(String.Format("{0}, Запись в журнале", DateTime.Now));
+
+            Assert.IsTrue(File.Exists("log-0.log"));
+            
+            FileInfo fileInfo = new FileInfo("log-0.log");
+            
+            Assert.IsTrue(fileInfo.Length > 10000);
+
+            Assert.IsTrue(File.Exists("log-1.log"));
+
+            StreamReader streamReader = new StreamReader(new FileStream("log-1.log", FileMode.Open, FileAccess.Read, FileShare.ReadWrite));
+            Assert.IsTrue(streamReader.ReadLine().Contains("Запись в журнале"));
+
+            streamReader.Close();
+            streamReader.Dispose();
+
+            logger.Dispose();
+        }
+    }
+}
---
+++ TRL.Common.Test/Configuration/StopPointsSettingsFactoryTests.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRL.Common.Models;
+using TRL.Common.Data;
+using TRL.Configuration
