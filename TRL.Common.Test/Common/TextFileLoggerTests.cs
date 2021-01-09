98571db82b79b3dddeae034389d319f97f563d3d
jandalee <92017819+jandalee@users.noreply.github.com>
2021-01-09 09:03:48 +0000

    more test cleanup

---
+++ TRL.Common.Test/Common/TextFileLoggerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using TRL.Logging;
+
+namespace TRL.Common.Test
+{
+    [TestClass]
+    public class TextFileLoggerTests
+    {
+        private TextFileLogger logger;
+
+        [TestInitialize]
+        public void Common_Setup()
+        {
+            this.logger = new TextFileLogger();
+        }
+
+        [TestCleanup]
+        public void Common_Teardown()
+        {
+            this.logger.Dispose();
+
+            File.Delete("default-0.log");
+            File.Delete("log-0.log");
+            File.Delete("log-1.log");
+        }
+
+        [TestMethod]
+        public void Common_LogMessage()
+        {
+            string message = String.Concat(DateTime.Now.ToLocalTime(), ";Тестовое сообщение в ж
