8b28248635cc273ad4599bef756251a2a49025f6
jandalee <92017819+jandalee@users.noreply.github.com>
2021-01-14 06:10:55 +0000

    Update CHANGELOG.md and README.md

---
+++ TRL.Common.Test/Data/TickCollectionFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRL.Common.Models;
+using TRL.Common.Data;
+using TRL.Common.TimeHelpers;
+using TRL.Common.Collections;
+using TRL.Transaction;
+
+namespace TRL.Common.Test
+{
+    [TestClass]
+    public class TickCollectionFactoryTests
+    {
+        private IDataContext tradingData;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.tradingData = new TradingDataContext();
+        }
+
+        [TestMethod]
+        public void TickCollectionFactory_Import()
+        {
+            IGenericFac
