ac9185ed4a3135d275d47ba2d5e7bb73b3f04cd9
jandalee <92017819+jandalee@users.noreply.github.com>
2021-01-26 15:52:10 +0000

    Add missing license header.

--- TRL.Common.Test/Extensions/Data/OrderBookContextExtensionsTests.cs
+++ TRL.Common.Test/Extensions/Data/OrderBookContextExtensionsTests.cs
@@ -11,4 +11,39 @@ namespace TRL.Common.Extensions.Data.Test
     [TestClass]
     public class OrderBookContextExtensionsTests
     {
-        private OrderBookContext orderBo
+        private OrderBookContext orderBook;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.orderBook = new OrderBookContext();
+
+            for (int i = 0; i < this.orderBook.Depth; i++)
+                this.orderBook.Update(i, "Any", i, 10, i + this.orderBook.Depth, 20);
+        }
+
+        [TestMethod]
+        public void OrderBookContextExtensions_GetOfferVolumeSum_test()
+        {
+            Assert.AreEqual(1000, this.orderBook.GetOfferVolumeSum("Any"));
+        }
+
+        [TestMethod]
+        public void OrderBookContextExtensions_GetBidVolumeSum_test()
+        {
+            Assert.AreEqual(500, this.orderBook.GetBidVolumeSum("Any"));
+        }
+
+        [TestMethod]
+        public void OrderBookContextExtensions_GetOfferVolumeSum_for_rows_test()
+        {
+            Assert.AreEqual(60, this.orderBook.GetOfferVolumeSum("Any", 3));
+        }
+
+        [TestMethod]
+        public void OrderBookContextExtensions_GetBidVolumeSum_for_rows_test()
+        {
+            Assert.AreEqual(50, this.orderBook.GetBidVolumeSum("Any", 5));
+        }
+    }
+}
