d524ed8f15e31b554bcbc8ebf5acf0b7f689546c
jandalee <92017819+jandalee@users.noreply.github.com>
2021-01-26 22:05:30 +0000

    updated by "~despintr/send_espresso.php" PHP-script

--- TRL.Common.Test/Extensions/Data/TradingDataContextGetCurrentPositionTradesTests.cs
+++ TRL.Common.Test/Extensions/Data/TradingDataContextGetCurrentPositionTradesTests.cs
@@ -137,4 +137,85 @@ namespace TRL.Common.Extensions.Data.Test
         [TestMethod]
         public void GetCurrentPositionTrades_for_second_short_position_with_multiple_trades_test()
         {
-            Signal firstSignal = new Signal(this.strategyHeader, DateTime.Now, T
+            Signal firstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 140000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 1);
+            this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 4);
+            this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 5);
+
+            Signal closeFirstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 141000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(closeFirstSignal);
+
+            Signal secondSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 141000, 0, 0);
+            Trade t1 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+            Trade t2 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+            Trade t3 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 3);
+            Trade t4 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 1);
+            Trade t5 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+
+            ICollection<Trade> trades = this.tradingData.GetPositionTrades(this.strategyHeader);
+            Assert.AreEqual(5, trades.Count);
+            Assert.AreEqual(t1.Id, trades.Last().Id);
+            Assert.AreEqual(t2.Id, trades.ElementAt(3).Id);
+            Assert.AreEqual(t3.Id, trades.ElementAt(2).Id);
+            Assert.AreEqual(t4.Id, trades.ElementAt(1).Id);
+            Assert.AreEqual(t5.Id, trades.First().Id);
+        }
+
+        [TestMethod]
+        public void GetCurrentPositionTrades_for_second_long_position_with_multiple_trades_close_partially_test()
+        {
+            Signal firstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 140000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 4);
+            Trade t0 = this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 6);
+
+            Signal closeFirstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 141000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(closeFirstSignal, 141000, 8);
+
+            Signal secondSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 141000, 0, 0);
+            Trade t1 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 7);
+            Trade t2 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 1);
+            Trade t3 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 1);
+            Trade t4 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 1);
+
+            ICollection<Trade> trades = this.tradingData.GetPositionTrades(this.strategyHeader);
+            Assert.AreEqual(5, trades.Count);
+            Assert.AreEqual(t0.Id, trades.Last().Id);
+            Assert.AreEqual(2, trades.Last().Amount);
+            Assert.AreEqual(t1.Id, trades.ElementAt(3).Id);
+            Assert.AreEqual(t2.Id, trades.ElementAt(2).Id);
+            Assert.AreEqual(t3.Id, trades.ElementAt(1).Id);
+            Assert.AreEqual(t4.Id, trades.First().Id);
+        }
+
+        [TestMethod]
+        public void GetCurrentPositionTrades_for_second_short_position_with_multiple_trades_close_partially_test()
+        {
+            Signal firstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 140000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 1);
+            Trade tp = this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 4);
+            Trade t0 = this.tradingData.AddSignalAndItsOrderAndTrade(firstSignal, 140000, 5);
+
+            Signal closeFirstSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 141000, 0, 0);
+            this.tradingData.AddSignalAndItsOrderAndTrade(closeFirstSignal, 141000, 3);
+
+            Signal secondSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 141000, 0, 0);
+            Trade t1 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+            Trade t2 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+            Trade t3 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 3);
+            Trade t4 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 1);
+            Trade t5 = this.tradingData.AddSignalAndItsOrderAndTrade(secondSignal, 141000, 2);
+
+            ICollection<Trade> trades = this.tradingData.GetPositionTrades(this.strategyHeader);
+            Assert.AreEqual(7, trades.Count);
+            Assert.AreEqual(tp.Id, trades.Last().Id);
+            Assert.AreEqual(-2, trades.Last().Amount);
+            Assert.AreEqual(t0.Id, trades.ElementAt(5).Id);
+            Assert.AreEqual(t1.Id, trades.ElementAt(4).Id);
+            Assert.AreEqual(t2.Id, trades.ElementAt(3).Id);
+            Assert.AreEqual(t3.Id, trades.ElementAt(2).Id);
+            Assert.AreEqual(t4.Id, trades.ElementAt(1).Id);
+            Assert.AreEqual(t5.Id, trades.First().Id);
+        }
+
+    }
+}
