9069a002c85bb386c95316deb56f3f7fc9d0eb42
jandalee <92017819+jandalee@users.noreply.github.com>
2021-03-05 23:05:50 +0000

    update test.html

--- TRL.Common.Test/Handlers/StopLoss/PlaceStrategyStopLossByPointsOnTradeMeasureFromTradePriceTests.cs
+++ TRL.Common.Test/Handlers/StopLoss/PlaceStrategyStopLossByPointsOnTradeMeasureFromTradePriceTests.cs
@@ -104,4 +104,189 @@ namespace TRL.Common.Handlers.Test.StopLoss
             Signal closeSignal = this.signalQueue.Dequeue();
             Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
             Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
-            Ass
+            Assert.AreEqual(TradeAction.Sell, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(firstTrade.Price, closeSignal.Price);
+            Assert.AreEqual(firstTrade.Price - this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(firstTrade.Amount + secondTrade.Amount, closeSignal.Amount);
+        }
+
+        [TestMethod]
+        public void make_stop_for_long_with_multiple_open_trades_when_order_is_canceled_before_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+
+            firstTrade.Order.Cancel(DateTime.Now, "cancel order");
+            Assert.IsTrue(firstTrade.Order.IsCanceled);
+
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price + 10, 3);
+            double amount = Math.Abs(this.tradingData.GetAmount(this.strategyHeader));
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Assert.AreEqual(TradeAction.Sell, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(secondTrade.Price, closeSignal.Price);
+            Assert.AreEqual(secondTrade.Price - this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(amount, closeSignal.Amount);
+        }
+
+
+        [TestMethod]
+        public void make_no_signal_for_long_with_multiple_open_trades_when_order_is_canceled_after_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 3);
+            double amount = Math.Abs(this.tradingData.GetAmount(this.strategyHeader));
+            firstTrade.Order.Cancel(DateTime.Now, "cancel order");
+            Assert.IsTrue(firstTrade.Order.IsCanceled);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void make_no_signal_for_long_when_unfilled_stop_order_exists_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Order stopOrder = this.tradingData.AddSignalAndItsOrder(this.signalQueue.Dequeue());
+
+            Signal reBuySignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(reBuySignal);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void make_no_signal_for_long_when_unfilled_market_order_exists_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+            Assert.AreEqual(1, this.signalQueue.Count);
+            this.signalQueue.Dequeue();
+
+            Signal marketSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            this.tradingData.AddSignalAndItsOrder(marketSignal);
+
+            Signal reBuySignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(reBuySignal);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+
+        [TestMethod]
+        public void make_signal_to_buy_on_stop_for_short_position_with_single_open_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade trade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Assert.AreEqual(TradeAction.Buy, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(trade.Price, closeSignal.Price);
+            Assert.AreEqual(trade.Price + this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(Math.Abs(trade.Amount), closeSignal.Amount);
+        }
+
+        [TestMethod]
+        public void make_signal_to_buy_on_stop_for_short_position_with_multiple_open_trades_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price - 10, 5);
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Assert.AreEqual(TradeAction.Buy, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(secondTrade.Price, closeSignal.Price);
+            Assert.AreEqual(secondTrade.Price + this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(Math.Abs(firstTrade.Amount + secondTrade.Amount), closeSignal.Amount);
+        }
+
+        [TestMethod]
+        public void make_stop_for_short_with_multiple_open_trades_when_order_is_canceled_before_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+
+            firstTrade.Order.Cancel(DateTime.Now, "cancel order");
+            Assert.IsTrue(firstTrade.Order.IsCanceled);
+
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price - 10, 3);
+            double amount = Math.Abs(this.tradingData.GetAmount(this.strategyHeader));
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Assert.AreEqual(TradeAction.Buy, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(secondTrade.Price, closeSignal.Price);
+            Assert.AreEqual(secondTrade.Price + this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(amount, closeSignal.Amount);
+        }
+
+
+        [TestMethod]
+        public void make_no_signal_for_short_with_multiple_open_trades_when_order_is_canceled_after_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 3);
+            double amount = Math.Abs(this.tradingData.GetAmount(this.strategyHeader));
+            firstTrade.Order.Cancel(DateTime.Now, "cancel order");
+            Assert.IsTrue(firstTrade.Order.IsCanceled);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void make_no_signal_for_short_when_unfilled_stop_order_exists_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Order stopOrder = this.tradingData.AddSignalAndItsOrder(this.signalQueue.Dequeue());
+
+            Signal reBuySignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(reBuySignal);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void make_no_signal_for_short_when_unfilled_market_order_exists_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+            Assert.AreEqual(1, this.signalQueue.Count);
+            this.signalQueue.Dequeue();
+
+            Signal marketSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            this.tradingData.AddSignalAndItsOrder(marketSignal);
+
+            Signal reBuySignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Sell, OrderType.Market, 125000, 0, 0);
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(reBuySignal);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+    }
+}
---
+++ TRL.Common.Test/Handlers/StopLoss/StrategiesPlaceStopLossByPointsOnTradeHandlersTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRL.Common.Data;
+using TRL.Common.Models;
+using System.Collections.Generic;
+using TRL.Common.Handlers;
+using TRL.Common.Collections;
+using TRL.Handlers.StopLoss;
+using TRL.Logging;
+
+namespace TRL.Common.Handlers.Test.StopLoss
+{
+    [TestClass]
+    public class StrategiesPlaceStopLossByPointsOnTradeHandlersTests
+    {
+        private IDataContext tradingData;
+        private ObservableQueue<Signal> signalQueue;
+        private int strategiesCounter, stopPointsSettingsCounter, stopLossOrderSettingsCounter;
+
+        private StrategiesPlaceStopLossByPointsOnTradeHandlers handlers;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.tradingData = new TradingDataContext();
+            this.signalQueue = new ObservableQueue<Signal>();
+            this.strategiesCounter = 5;
+            this.stopPointsSettingsCounter = 4;
+            this.stopLossOrderSettingsCounter = 3;
+
+            MakeAndAddStrategiesToTradingDataContext(this.strategiesCounter);
+            Assert.AreEqual(this.strategiesCounter, this.tradingData.Get<IEnumerable<StrategyHeader>>().Count());
+
+            MakeAndAddStopPointsSettingsToTradingDataContext(this.stopPointsSettingsCounter);
+            Assert.AreEqual(this.stopPointsSettingsCounter, this.tradingData.Get<IEnumerable<StopPointsSettings>>().Count());
+
+            MakeAndAddStopLossOrderSettingsToTradingDataContext(this.stopLossOrderSettingsCounter);
+            Assert.AreEqual(this.stopLossOrderSettingsCounter, this.tradingData.Get<IEnumerable<StopLossOrderSettings>>().Count());
+
+            this.handlers = 
+                new StrategiesPlaceStopLossByPointsOnTradeHandlers(this.tradingData, this.signalQueue, new NullLogger());
+        }
+
+        private void
