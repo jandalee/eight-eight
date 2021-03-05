f662a70d3f216feda7cefcb068be7004cc93054b
jandalee <92017819+jandalee@users.noreply.github.com>
2021-03-05 20:16:41 +0000

    More build fixes.

--- TRL.Common.Test/Handlers/Spreads/ExitSpreadOnQuoteChangeTests.cs
+++ TRL.Common.Test/Handlers/Spreads/ExitSpreadOnQuoteChangeTests.cs
@@ -142,4 +142,61 @@ namespace TRL.Common.Handlers.Test.Spreads
             StrategyHeader strtgy = this.tradingData.Get<IEnumerable<StrategyHeader>>().Single(s => s.Id == 1);
 
             Signal sgnl = new Signal(strtgy, BrokerDateTime.Make(DateTime.Now), TradeAction.Sell, OrderType.Market, 140000, 0, 0);
-            this.tradingData.Get<ICollection<Signal>>().Add
+            this.tradingData.Get<ICollection<Signal>>().Add(sgnl);
+
+            Order ordr = new Order(sgnl);
+            this.tradingData.Get<ICollection<Order>>().Add(ordr);
+
+            OrderDeliveryConfirmation cnfrmtn = new OrderDeliveryConfirmation(ordr, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<OrderDeliveryConfirmation>>().Add(cnfrmtn);
+            Assert.IsTrue(ordr.IsDelivered);
+
+            Trade trd = new Trade(ordr, ordr.Portfolio, ordr.Symbol, 1430000, -ordr.Amount, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<Trade>>().Add(trd);
+            Assert.AreEqual(1, this.tradingData.Get<IEnumerable<Position>>().Count());
+
+
+            StrategyHeader strategy2 = this.tradingData.Get<IEnumerable<StrategyHeader>>().Single(s => s.Id == 2);
+
+            Signal signal2 = new Signal(strategy2, BrokerDateTime.Make(DateTime.Now), TradeAction.Buy, OrderType.Market, 31000, 0, 0);
+            this.tradingData.Get<ICollection<Signal>>().Add(signal2);
+
+            Order order2 = new Order(signal2);
+            this.tradingData.Get<ICollection<Order>>().Add(order2);
+
+            OrderDeliveryConfirmation confirmation2 = new OrderDeliveryConfirmation(order2, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<OrderDeliveryConfirmation>>().Add(confirmation2);
+            Assert.IsTrue(order2.IsDelivered);
+
+            Trade trade2 = new Trade(order2, order2.Portfolio, order2.Symbol, 31000, order2.Amount, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<Trade>>().Add(trade2);
+            Assert.AreEqual(2, this.tradingData.Get<IEnumerable<Position>>().Count());
+
+
+            StrategyHeader strategy3 = this.tradingData.Get<IEnumerable<StrategyHeader>>().Single(s => s.Id == 3);
+
+            Signal signal3 = new Signal(strategy3, BrokerDateTime.Make(DateTime.Now), TradeAction.Buy, OrderType.Market, 41000, 0, 0);
+            this.tradingData.Get<ICollection<Signal>>().Add(signal3);
+
+            Order order3 = new Order(signal3);
+            this.tradingData.Get<ICollection<Order>>().Add(order3);
+
+            OrderDeliveryConfirmation confirmation3 = new OrderDeliveryConfirmation(order3, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<OrderDeliveryConfirmation>>().Add(confirmation3);
+            Assert.IsTrue(order3.IsDelivered);
+
+            Trade trade3 = new Trade(order3, order3.Portfolio, order3.Symbol, 41000, order3.Amount, BrokerDateTime.Make(DateTime.Now));
+            this.tradingData.Get<ObservableHashSet<Trade>>().Add(trade3);
+            Assert.AreEqual(3, this.tradingData.Get<IEnumerable<Position>>().Count());
+
+
+            this.qProvider.Update(0, "RTS-12.13_FT", 145000, 20, 145010, 100);
+            this.qProvider.Update(0, "Si-12.13_FT", 31999, 50, 32000, 40);
+            this.qProvider.Update(0, "Eu-12.13_FT", 43999, 30, 43000, 0);
+
+            Assert.AreEqual(3, this.tradingData.Get<IEnumerable<Signal>>().Count());
+            Assert.AreEqual(3, this.tradingData.Get<IEnumerable<Order>>().Count());
+        }
+
+    }
+}
---
+++ TRL.Common.Test/Handlers/StopLoss/PlaceStrategyStopLossByPointsOnTradeMeasureFromTradePriceTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRL.Common.Data;
+using TRL.Common.Collections;
+using TRL.Common.Models;
+using System.Collections.Generic;
+using TRL.Emulation;
+using TRL.Handlers.StopLoss;
+using TRL.Common.Extensions.Data;
+using TRL.Logging;
+
+namespace TRL.Common.Handlers.Test.StopLoss
+{
+    [TestClass]
+    public class PlaceStrategyStopLossByPointsOnTradeMeasureFromTradePriceTests
+    {
+        private IDataContext tradingData;
+        private ObservableQueue<Signal> signalQueue;
+        private StrategyHeader strategyHeader;
+        private StopPointsSettings spSettings;
+        private StopLossOrderSettings slSettings;
+
+        private PlaceStrategyStopLossByPointsOnTrade handler;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.tradingData = new TradingDataContext();
+            this.signalQueue = new ObservableQueue<Signal>();
+
+            this.strategyHeader = new StrategyHeader(1, "Description", "ST12345-RF-01", "RTS-9.14", 10);
+            this.tradingData.Get<ICollection<StrategyHeader>>().Add(this.strategyHeader);
+
+            this.spSettings = new StopPointsSettings(this.strategyHeader, 100, false);
+            this.tradingData.Get<ICollection<StopPointsSettings>>().Add(this.spSettings);
+
+            this.slSettings = new StopLossOrderSettings(this.strategyHeader, 180);
+            this.tradingData.Get<ICollection<StopLossOrderSettings>>().Add(this.slSettings);
+
+            this.handler =
+                new PlaceStrategyStopLossByPointsOnTrade(this.strategyHeader, this.tradingData, this.signalQueue, new NullLogger());
+
+            Assert.AreEqual(0, this.tradingData.Get<IEnumerable<Order>>().Count());
+            Assert.AreEqual(0, this.tradingData.Get<IEnumerable<Trade>>().Count());
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void PlaceStrategyStopLossByPointsOnTrade_is_Identified_test()
+        {
+            Assert.IsInstanceOfType(this.handler, typeof(IIdentified));
+            Assert.AreEqual(this.strategyHeader.Id, this.handler.Id);
+        }
+
+        private Trade MakeTrade()
+        {
+            Trade trade = new Trade();
+            trade.Id = 1;
+            trade.Portfolio = "ST12345-RF-01";
+            trade.Symbol = "RTS-9.14";
+            trade.Amount = 9;
+            trade.Price = 120000;
+            return trade;
+        }
+
+        [TestMethod]
+        public void do_nothing_when_here_is_no_signal_and_order_for_trade_test()
+        {
+            Trade trade = MakeTrade();
+
+            this.tradingData.Get<ObservableHashSet<Trade>>().Add(trade);
+            Assert.AreEqual(0, this.signalQueue.Count);
+        }
+
+        [TestMethod]
+        public void make_signal_to_sell_on_stop_for_long_position_with_single_open_trade_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade trade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal);
+
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Assert.AreEqual(TradeAction.Sell, closeSignal.TradeAction);
+            Assert.AreEqual(OrderType.Stop, closeSignal.OrderType);
+            Assert.AreEqual(trade.Price, closeSignal.Price);
+            Assert.AreEqual(trade.Price - this.spSettings.Points, closeSignal.Stop);
+            Assert.AreEqual(trade.Amount, closeSignal.Amount);
+        }
+
+        [TestMethod]
+        public void make_signal_to_sell_on_stop_for_long_position_with_multiple_open_trades_test()
+        {
+            Signal openSignal = new Signal(this.strategyHeader, DateTime.Now, TradeAction.Buy, OrderType.Market, 125000, 0, 0);
+            Trade firstTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(0, this.signalQueue.Count);
+            
+            Trade secondTrade = this.tradingData.AddSignalAndItsOrderAndTrade(openSignal, openSignal.Price, 5);
+            Assert.AreEqual(1, this.signalQueue.Count);
+
+            Signal closeSignal = this.signalQueue.Dequeue();
+            Assert.AreEqual(this.strategyHeader.Id, closeSignal.StrategyId);
+            Assert.AreEqual(this.strategyHeader, closeSignal.Strategy);
+            Ass
