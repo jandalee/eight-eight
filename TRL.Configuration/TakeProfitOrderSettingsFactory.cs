40deb581cf958bf7a3ce2390b46a10b2a50a4345
jandalee <92017819+jandalee@users.noreply.github.com>
2021-09-14 13:58:54 +0000

    preparing for merge

---
+++ TRL.Configuration/TakeProfitOrderSettingsFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TRL.Common.Models;
+using TRL.Common.Data;
+using TRL.Common;
+
+namespace TRL.Configuration
+{
+    public class TakeProfitOrderSettingsFactory:IGenericFactory<TakeProfitOrderSettings>
+    {
+        private StrategyHeader strategyHeader;
+        private string prefix;
+
+        public TakeProfitOrderSettingsFactory(StrategyHeader strategyHeader, string prefix)
+        {
+            this.strategyHeader = strategyHeader;
+            thi
