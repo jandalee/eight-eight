a2bb84400e198a4df46d2fccf87313872adf1e4d
jandalee <92017819+jandalee@users.noreply.github.com>
2021-10-03 19:43:53 +0000

    Update Readme.md [skip ci]

--- TRL.Csharp.Test/Models/TradeTests.cs
+++ TRL.Csharp.Test/Models/TradeTests.cs
@@ -70,4 +70,125 @@ namespace TRL.Csharp.Test.Models
         public void constructor_with_id_test()
         {
             int id = 10;
-            DateTime date = DateTime.N
+            DateTime date = DateTime.Now;
+            double price = 125000;
+            double amount = -5;
+            string portfolio = "ST12345-RF-01";
+            string symbol = "RTS-9.14";
+
+            Trade trade = new Trade(id, portfolio, symbol, date, price, amount);
+            Assert.AreEqual(id, trade.Id);
+            Assert.AreEqual(date, trade.DateTime);
+            Assert.AreEqual(price, trade.Price);
+            Assert.AreEqual(amount, trade.Amount);
+            Assert.AreEqual(portfolio, trade.Portfolio);
+            Assert.AreEqual(symbol, trade.Symbol);
+        }
+
+        [TestMethod]
+        public void link_Order_with_Trade_test()
+        {
+            Order order =
+                new Order(1,
+                    DateTime.Now,
+                    "ST12345-RF-01",
+                    "RTS-9.14",
+                    OrderType.Market,
+                    123000,
+                    10);
+
+            Trade trade = new Trade(1,
+                order.Portfolio,
+                order.Symbol,
+                DateTime.Now,
+                123010,
+                10);
+
+            trade.Order = order;
+            Assert.AreEqual(order, trade.Order);
+        }
+
+        [TestMethod]
+        public void do_not_link_Order_with_inappropriate_Portfolio_with_Trade_test()
+        {
+            Order order =
+                new Order(1,
+                    DateTime.Now,
+                    "ST12346-RF-01",
+                    "RTS-12.14",
+                    OrderType.Market,
+                    123000,
+                    10);
+
+            Trade trade = new Trade(1,
+                "ST12345-RF-01",
+                order.Symbol,
+                DateTime.Now,
+                123010,
+                10);
+
+            trade.Order = order;
+            Assert.IsNull(trade.Order);
+        }
+
+        [TestMethod]
+        public void do_not_link_Order_with_inappropriate_Symbol_with_Trade_test()
+        {
+            Order order =
+                new Order(1,
+                    DateTime.Now,
+                    "ST12346-RF-01",
+                    "RTS-12.14",
+                    OrderType.Market,
+                    123000,
+                    10);
+
+            Trade trade = new Trade(1,
+                order.Portfolio,
+                "RTS-9.14",
+                DateTime.Now,
+                123010,
+                10);
+
+            trade.Order = order;
+            Assert.IsNull(trade.Order);
+        }
+
+        [TestMethod]
+        public void do_not_link_Order_with_Trade_twice_test()
+        {
+            Order order =
+                new Order(1,
+                    DateTime.Now,
+                    "ST12346-RF-01",
+                    "RTS-12.14",
+                    OrderType.Market,
+                    123000,
+                    10);
+
+            Trade trade = new Trade(1,
+                order.Portfolio,
+                order.Symbol,
+                DateTime.Now,
+                123010,
+                10);
+
+            trade.Order = order;
+            Assert.AreEqual(order, trade.Order);
+
+
+            Order secondOrder =
+                new Order(1,
+                    DateTime.Now,
+                    "ST12346-RF-01",
+                    "RTS-12.14",
+                    OrderType.Market,
+                    123000,
+                    10);
+
+            trade.Order = secondOrder;
+            Assert.AreEqual(order, trade.Order);
+        }
+
+    }
+}
---
+++ TRL.Emulation/BacktestOrderManager.cs
@@ -0,0 +1,40 @@
+﻿using TRL.Common;
+using TRL.Common.Collections;
+using TRL.Common.Data;
+using TRL.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRL.Logging;
+using TRL.Common.Handlers;
+
+namespace TRL.Emulation
+{
+    public class BacktestOrderManager:IOrderManager
+    {
+        private IDataContext tradingData;
+        private ILogger logger;
+
+        public BacktestOrderManager(IDataContext tradingData, ILogger logger)
+        {
+            this.tradingData = tradingData;
+            this.logger = logger;
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            Bar lastBar = 
+                this.tradingData.Get<IEnumerable<Bar>>().LastOrDefault(b => b.Symbol.Equals(order.Symbol));
+
+            if (lastBar == null)
+                return;
+
+            Trade trade =
+                new Trade(order,
+                    order.Portfolio,
+                    order.Symbol,
+                    lastBar.Close,
+                    order.TradeAction == TradeAction.Buy? order.Amount:-order.Amount,
+      
