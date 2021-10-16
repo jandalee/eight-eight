4f8950408ca49d876bade634033eda89689c4fc2
jandalee <92017819+jandalee@users.noreply.github.com>
2021-10-16 06:26:04 +0000

    Remove debug log

--- TRS.Trader/TRS.Trader.Scalper.Test/FollowTrendOnTickTests.cs
+++ TRS.Trader/TRS.Trader.Scalper.Test/FollowTrendOnTickTests.cs
@@ -25,4 +25,46 @@ namespace TRx.Trader.Scalper.Test
             this.orderBook = new OrderBookContext();
             this.tradingData.Get<ObservableCollection<Tick>>().Add(new Tick("RTS-12.13_FT", new DateTime(2013, 12, 3, 10, 0, 0), 138000, 3600));
 
-            this.orderBook.Update(0, this.strategyHeader.Symbol, 138000, 100,
+            this.orderBook.Update(0, this.strategyHeader.Symbol, 138000, 100, 138010, 50);
+
+            FollowTrendOnTick handler =
+                new FollowTrendOnTick(this.strategyHeader, 
+                    30,
+                    50,
+                    this.orderBook, 
+                    this.tradingData, 
+                    this.signalQueue, 
+                    new NullLogger());
+        }
+
+        [TestMethod]
+        public void open_long_position_on_tick_test()
+        {
+            Assert.AreEqual(0, this.tradingData.Get<IEnumerable<Order>>().Count());
+
+            this.tradingData.Get<ObservableCollection<Tick>>().Add(new Tick("RTS-12.13_FT", new DateTime(2013, 12, 3, 10, 0, 1), 138090, 3000, TradeAction.Sell));
+
+            Assert.AreEqual(1, this.tradingData.Get<IEnumerable<Order>>().Count());
+
+            Order order = this.tradingData.Get<IEnumerable<Order>>().Last();
+            Assert.AreEqual(TradeAction.Buy, order.TradeAction);
+            Assert.AreEqual(OrderType.Limit, order.OrderType);
+            Assert.AreEqual(138010, order.Price);
+        }
+
+        [TestMethod]
+        public void open_short_position_on_tick_test()
+        {
+            Assert.AreEqual(0, this.tradingData.Get<IEnumerable<Order>>().Count());
+
+            this.tradingData.Get<ObservableCollection<Tick>>().Add(new Tick("RTS-12.13_FT", new DateTime(2013, 12, 3, 10, 0, 1), 137950, 3000, TradeAction.Sell));
+
+            Assert.AreEqual(1, this.tradingData.Get<IEnumerable<Order>>().Count());
+
+            Order order = this.tradingData.Get<IEnumerable<Order>>().Last();
+            Assert.AreEqual(TradeAction.Sell, order.TradeAction);
+            Assert.AreEqual(OrderType.Limit, order.OrderType);
+            Assert.AreEqual(138000, order.Price);
+        }
+    }
+}
---
+++ TRS.Trader/TRS.Trader.Scalper.Test/TradingDataContextTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TRL.Common.Data;
+using TRL.Common.Models;
+using System.Collections.Generic;
+using TRL.Common.Collections;
+
+namespace TRx.Trader.Scalper.Test
+{
+    public class Customer
+    {
+    }
+
+    [TestClass]
+    public class TradingDataContextTests
+    {
+        private IDataContext tradingData;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.tradingData = new TradingDataContext();
+        }
+
+        [TestMethod]
+        public void How_to_add_strategy_to_data_context_test()
+        {
+            Assert.AreEqual(0, this.tradingData.Get<IEnumerable<StrategyHeader>>().Count());
+
+            StrategyHeader strategyHeader = new StrategyHeader(1, "Strategy", "BP12345-RF-01", "RTS-3.14_FT", 10);
+            
+            this.tradingData.Get<ICollection<StrategyHeader>>().Add(strategyHeader);
+
+            Assert.AreEqual(1, this.tradingData.Get<IEnumerable<StrategyHeader>>().Count());
+        }
+
+        [TestMethod]
+        public void Request_customers_collection_returns_null_test()
+        {
+            Assert.IsNull(this.tradingData.Get<IEnu
