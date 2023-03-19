bbf1df58792a6615f0a78090037182d30fa50962
jandalee <92017819+jandalee@users.noreply.github.com>
2023-03-19 23:11:32 +0000

    moved repo

--- TRS.Trader/TRS.Trader.WebClient.highstock/js/jquery-2.0.2-vsdoc.js
+++ TRS.Trader/TRS.Trader.WebClient.highstock/js/jquery-2.0.2-vsdoc.js
@@ -361,4 +361,387 @@
 										.fail(newDefer.reject)
 										.progress(newDefer.notify);
 			                    } else {
-			                        newDefer[action + "With"](this === promise ? newDefer.promise() : this, 
+			                        newDefer[action + "With"](this === promise ? newDefer.promise() : this, fn ? [returned] : arguments);
+			                    }
+			                });
+			            });
+			            fns = null;
+			        }).promise();
+			    },
+			    // Get a promise for this deferred
+			    // If obj is provided, the promise aspect is added to the object
+			    promise: function (obj) {
+			        return obj != null ? jQuery.extend(obj, promise) : promise;
+			    }
+			},
+			deferred = {};
+
+        // Keep pipe for back-compat
+        promise.pipe = promise.then;
+
+        // Add list-specific methods
+        jQuery.each(tuples, function (i, tuple) {
+            var list = tuple[2],
+				stateString = tuple[3];
+
+            // promise[ done | fail | progress ] = list.add
+            promise[tuple[1]] = list.add;
+
+            // Handle state
+            if (stateString) {
+                list.add(function () {
+                    // state = [ resolved | rejected ]
+                    state = stateString;
+
+                    // [ reject_list | resolve_list ].disable; progress_list.lock
+                }, tuples[i ^ 1][2].disable, tuples[2][2].lock);
+            }
+
+            // deferred[ resolve | reject | notify ]
+            deferred[tuple[0]] = function () {
+                deferred[tuple[0] + "With"](this === deferred ? promise : this, arguments);
+                return this;
+            };
+            deferred[tuple[0] + "With"] = list.fireWith;
+        });
+
+        // Make the deferred a promise
+        promise.promise(deferred);
+
+        // Call given func if any
+        if (func) {
+            func.call(deferred, deferred);
+        }
+
+        // All done!
+        return deferred;
+    };
+    jQuery.Event = function (src, props) {
+
+        // Allow instantiation without the 'new' keyword
+        if (!(this instanceof jQuery.Event)) {
+            return new jQuery.Event(src, props);
+        }
+
+        // Event object
+        if (src && src.type) {
+            this.originalEvent = src;
+            this.type = src.type;
+
+            // Events bubbling up the document may have been marked as prevented
+            // by a handler lower down the tree; reflect the correct value.
+            this.isDefaultPrevented = (src.defaultPrevented ||
+                src.getPreventDefault && src.getPreventDefault()) ? returnTrue : returnFalse;
+
+            // Event type
+        } else {
+            this.type = src;
+        }
+
+        // Put explicitly provided properties onto the event object
+        if (props) {
+            jQuery.extend(this, props);
+        }
+
+        // Create a timestamp if incoming event doesn't have one
+        this.timeStamp = src && src.timeStamp || jQuery.now();
+
+        // Mark it as fixed
+        this[jQuery.expando] = true;
+    };
+    jQuery.Tween = function Tween(elem, options, prop, end, easing) {
+
+        return new Tween.prototype.init(elem, options, prop, end, easing);
+    };
+    jQuery._data = function (elem, name, data) {
+
+        return data_priv.access(elem, name, data);
+    };
+    jQuery._evalUrl = function (url) {
+
+        return jQuery.ajax({
+            url: url,
+            type: "GET",
+            dataType: "text",
+            async: false,
+            global: false,
+            success: jQuery.globalEval
+        });
+    };
+    jQuery._queueHooks = function (elem, type) {
+
+        var key = type + "queueHooks";
+        return data_priv.get(elem, key) || data_priv.access(elem, key, {
+            empty: jQuery.Callbacks("once memory").add(function () {
+                data_priv.remove(elem, [type + "queue", key]);
+            })
+        });
+    };
+    jQuery._removeData = function (elem, name) {
+
+        data_priv.remove(elem, name);
+    };
+    jQuery.acceptData = function (owner) {
+
+        // Accepts only:
+        //  - Node
+        //    - Node.ELEMENT_NODE
+        //    - Node.DOCUMENT_NODE
+        //  - Object
+        //    - Any
+        return owner.nodeType ?
+            owner.nodeType === 1 || owner.nodeType === 9 : true;
+    };
+    jQuery.access = function (elems, fn, key, value, chainable, emptyGet, raw) {
+
+        var i = 0,
+			length = elems.length,
+			bulk = key == null;
+
+        // Sets many values
+        if (jQuery.type(key) === "object") {
+            chainable = true;
+            for (i in key) {
+                jQuery.access(elems, fn, i, key[i], true, emptyGet, raw);
+            }
+
+            // Sets one value
+        } else if (value !== undefined) {
+            chainable = true;
+
+            if (!jQuery.isFunction(value)) {
+                raw = true;
+            }
+
+            if (bulk) {
+                // Bulk operations run against the entire set
+                if (raw) {
+                    fn.call(elems, value);
+                    fn = null;
+
+                    // ...except when executing function values
+                } else {
+                    bulk = fn;
+                    fn = function (elem, key, value) {
+                        return bulk.call(jQuery(elem), value);
+                    };
+                }
+            }
+
+            if (fn) {
+                for (; i < length; i++) {
+                    fn(elems[i], key, raw ? value : value.call(elems[i], i, fn(elems[i], key)));
+                }
+            }
+        }
+
+        return chainable ?
+            elems :
+
+			// Gets
+			bulk ?
+				fn.call(elems) :
+				length ? fn(elems[0], key) : emptyGet;
+    };
+    jQuery.active = 0;
+    jQuery.ajax = function (url, options) {
+        /// <summary>
+        ///     Perform an asynchronous HTTP (Ajax) request.
+        ///     &#10;1 - jQuery.ajax(url, settings) 
+        ///     &#10;2 - jQuery.ajax(settings)
+        /// </summary>
+        /// <param name="url" type="String">
+        ///     A string containing the URL to which the request is sent.
+        /// </param>
+        /// <param name="options" type="PlainObject">
+        ///     A set of key/value pairs that configure the Ajax request. All settings are optional. A default can be set for any option with $.ajaxSetup(). See jQuery.ajax( settings ) below for a complete list of all settings.
+        /// </param>
+
+
+        // If url is an object, simulate pre-1.5 signature
+        if (typeof url === "object") {
+            options = url;
+            url = undefined;
+        }
+
+        // Force options to be an object
+        options = options || {};
+
+        var transport,
+			// URL without anti-cache param
+			cacheURL,
+			// Response headers
+			responseHeadersString,
+			responseHeaders,
+			// timeout handle
+			timeoutTimer,
+			// Cross-domain detection vars
+			parts,
+			// To know if global events are to be dispatched
+			fireGlobals,
+			// Loop variable
+			i,
+			// Create the final options object
+			s = jQuery.ajaxSetup({}, options),
+			// Callbacks context
+			callbackContext = s.context || s,
+			// Context for global events is callbackContext if it is a DOM node or jQuery collection
+			globalEventContext = s.context && (callbackContext.nodeType || callbackContext.jquery) ?
+				jQuery(callbackContext) :
+				jQuery.event,
+			// Deferreds
+			deferred = jQuery.Deferred(),
+			completeDeferred = jQuery.Callbacks("once memory"),
+			// Status-dependent callbacks
+			statusCode = s.statusCode || {},
+			// Headers (they are sent all at once)
+			requestHeaders = {},
+			requestHeadersNames = {},
+			// The jqXHR state
+			state = 0,
+			// Default abort message
+			strAbort = "canceled",
+			// Fake xhr
+			jqXHR = {
+			    readyState: 0,
+
+			    // Builds headers hashtable if needed
+			    getResponseHeader: function (key) {
+			        var match;
+			        if (state === 2) {
+			            if (!responseHeaders) {
+			                responseHeaders = {};
+			                while ((match = rheaders.exec(responseHeadersString))) {
+			                    responseHeaders[match[1].toLowerCase()] = match[2];
+			                }
+			            }
+			            match = responseHeaders[key.toLowerCase()];
+			        }
+			        return match == null ? null : match;
+			    },
+
+			    // Raw string
+			    getAllResponseHeaders: function () {
+			        return state === 2 ? responseHeadersString : null;
+			    },
+
+			    // Caches the header
+			    setRequestHeader: function (name, value) {
+			        var lname = name.toLowerCase();
+			        if (!state) {
+			            name = requestHeadersNames[lname] = requestHeadersNames[lname] || name;
+			            requestHeaders[name] = value;
+			        }
+			        return this;
+			    },
+
+			    // Overrides response content-type header
+			    overrideMimeType: function (type) {
+			        if (!state) {
+			            s.mimeType = type;
+			        }
+			        return this;
+			    },
+
+			    // Status-dependent callbacks
+			    statusCode: function (map) {
+			        var code;
+			        if (map) {
+			            if (state < 2) {
+			                for (code in map) {
+			                    // Lazy-add the new callback in a way that preserves old ones
+			                    statusCode[code] = [statusCode[code], map[code]];
+			                }
+			            } else {
+			                // Execute the appropriate callbacks
+			                jqXHR.always(map[jqXHR.status]);
+			            }
+			        }
+			        return this;
+			    },
+
+			    // Cancel the request
+			    abort: function (statusText) {
+			        var finalText = statusText || strAbort;
+			        if (transport) {
+			            transport.abort(finalText);
+			        }
+			        done(0, finalText);
+			        return this;
+			    }
+			};
+
+        // Attach deferreds
+        deferred.promise(jqXHR).complete = completeDeferred.add;
+        jqXHR.success = jqXHR.done;
+        jqXHR.error = jqXHR.fail;
+
+        // Remove hash character (#7531: and string promotion)
+        // Add protocol if not provided (prefilters might expect it)
+        // Handle falsy url in the settings object (#10093: consistency with old signature)
+        // We also use the url parameter if available
+        s.url = ((url || s.url || ajaxLocation) + "").replace(rhash, "")
+			.replace(rprotocol, ajaxLocParts[1] + "//");
+
+        // Alias method option to type as per ticket #12004
+        s.type = options.method || options.type || s.method || s.type;
+
+        // Extract dataTypes list
+        s.dataTypes = jQuery.trim(s.dataType || "*").toLowerCase().match(core_rnotwhite) || [""];
+
+        // A cross-domain request is in order when we have a protocol:host:port mismatch
+        if (s.crossDomain == null) {
+            parts = rurl.exec(s.url.toLowerCase());
+            s.crossDomain = !!(parts &&
+				(parts[1] !== ajaxLocParts[1] || parts[2] !== ajaxLocParts[2] ||
+					(parts[3] || (parts[1] === "http:" ? "80" : "443")) !==
+						(ajaxLocParts[3] || (ajaxLocParts[1] === "http:" ? "80" : "443")))
+			);
+        }
+
+        // Convert data if not already a string
+        if (s.data && s.processData && typeof s.data !== "string") {
+            s.data = jQuery.param(s.data, s.traditional);
+        }
+
+        // Apply prefilters
+        inspectPrefiltersOrTransports(prefilters, s, options, jqXHR);
+
+        // If request was aborted inside a prefilter, stop there
+        if (state === 2) {
+            return jqXHR;
+        }
+
+        // We can fire global events as of now if asked to
+        fireGlobals = s.global;
+
+        // Watch for a new set of requests
+        if (fireGlobals && jQuery.active++ === 0) {
+            jQuery.event.trigger("ajaxStart");
+        }
+
+        // Uppercase the type
+        s.type = s.type.toUpperCase();
+
+        // Determine if request has content
+        s.hasContent = !rnoContent.test(s.type);
+
+        // Save the URL in case we're toying with the If-Modified-Since
+        // and/or If-None-Match header later on
+        cacheURL = s.url;
+
+        // More options handling for requests with no content
+        if (!s.hasContent) {
+
+            // If data is available, append data to url
+            if (s.data) {
+                cacheURL = (s.url += (ajax_rquery.test(cacheURL) ? "&" : "?") + s.data);
+                // #9682: remove data so that it's not used in an eventual retry
+                delete s.data;
+            }
+
+            // Add anti-cache in url if needed
+            if (s.cache === false) {
+                s.url = rts.test(cacheURL) ?
+
+					// If there i
