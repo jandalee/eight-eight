d6851f5bc11d158e49e1f7a42600a818051f9e0e
jandalee <92017819+jandalee@users.noreply.github.com>
2023-04-25 20:14:06 +0000

    Run update-remote.sh on influxdb

---
+++ TRx.Base/ISetupStrategy.cs
@@ -0,0 +1,13 @@
+
+﻿namespace TRx.Base
+{
+    /// <summary>
+    /// Initialize()
+    /// SetupStrategy(string[] args)
+    /// </summary>
+    public interface ISetupStrategy
+    {
+        void Initialize();
+        void SetupStrategy(string[] args);
+    }
+}
