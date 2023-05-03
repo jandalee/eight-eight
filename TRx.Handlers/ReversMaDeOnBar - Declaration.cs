c73e9ad9983787494c66e9ebab5fd4bd880470bf
jandalee <92017819+jandalee@users.noreply.github.com>
2023-05-03 17:46:48 +0000

    TODO

---
+++ TRx.Handlers/ReversMaDeOnBar - Declaration.cs
@@ -0,0 +1,171 @@
+
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TRL.Common;
+using TRL.Common.Collections;
+using TRL.Common.Extensions.Collections;
+using TRL.Common.Data;
+using TRL.Common.Extensions.Data;
+using TRL.Common.Handlers;
+using TRL.Common.Models;
+using TRL.Logging;
+using TRL.Common.TimeHelpers;
+using TRL.Common.Events;
+using TRx.Indicators;
+using TRx.Helpers;
+
+namespace TRx.Handlers
+{
+    /// <summary>
+    /// Возврат к средней на барах
+    /// </summary>
+    public partial class ReversMaDeOnBar//:AddedItemHandler<Bar>
+    {
+        // -------------------------------------------------
+        #region // TRL
+        // -------------------------------------------------
+        private StrategyHeader strategyHeader;
+        private IDataContext tradingData;
+        private ObservableQueue<Signal> signalQueue;
+        private ILogger logger;
+        // -------------------------------------------------
+        #endregion //
+        // -------------------------------------------------
+
+        // -------------------------------------------------
+        #region // Свойства
+        // -------------------------------------------------
+
+        /// <summary>
+        /// Индикатор отклонения от скользящей средней
+        /// </summary>
+        public IndicatorOnBarMaDeviation maDeviation { get; private set; }
+        /// <summary>
+        /// Индикатор уровни
+        /// </summary>
+        public Levels Levels { get; private set; }
+        /// <summary>
+        /// Номер пересеченного уровня для каждого бара
+        /// </summary>
+        public IList<int> Уровень { get; private set; }
+        /// <summary>
+        /// Номера прошлого пересеченного уровня для каждого бара
+        /// </summary>
+        public IList<int> УровеньПрошлый { get; private set; }
+        /// <summary>
+        /// Наличие пересечения по направлению
+        /// </summary>
+        public IList<bool> ПересечениeСверху { get; private set; }
+        /// <summary>
+        /// Наличие пересечения по направлению
+        /// </summary>
+        public IList<bool> ПересечениеСнизу { get; private set; }
+        /// <summary>
+        /// Признак На Открытие при пересечении уровней выше сеседины
+        /// </summary>
+        public IList<bool> ОткрытиеВерхний { get; private set; }
+        /// <summary>
+        /// КоличествоУровней пройдено при пересечении уровней выше сеседины
+        /// </summary>
+        public IList<int> ОткрытиеВерхнийКоличествоУровней { get; private set; }
+        /// <summary>
+        /// Признак На Открытие при пересечении уровней ниже середины
+        /// </summary>
+        public IList<bool> ОткрытиеНижний { get; private set; }
+        /// <summary>
+        /// КоличествоУровней пройдено при пересечении уровней ниже середины
+        /// </summary>
+        public IList<int> ОткрытиеНижнийКоличествоУровней { get; private set; }
+        /// <summary>
+        /// Признак На Закрытие при пересечении уровней выше середины
+        /// </summary>
+        public IList<bool> ЗакрытиеВерхний { get; private set; }
+        /// <summary>
+        /// КоличествоУровней пройдено при пересечении уровней выше середины
+        /// </summary>
+        public IList<int> ЗакрытиеВерхнийКоличествоУровней { get; private set; }
+        /// <summary>
+        /// Признак На Закрытие при пересечении уровней ниже середины
+        /// </summary>
+        public IList<bool> ЗакрытиеНижний { get; private set; }
+        /// <summary>
+        /// КоличествоУровней пройдено при пересечении уровней ниже середины
+        /// </summary>
+        public IList<int> ЗакрытиеНижнийКоличествоУровней { get; private set; }
+        /// <summary>
+        /// список стеков
+        /// </summary>
+        public IList<Stack<IList<Signal>>> LevelStack { get; private set; }
+        /// <summary>
+        /// список позиции
+        /// </summary>
+        public IList<Signal> Sell { get; private set; }
+        /// <summary>
+        /// список позиции
+        /// </summary>
+        public IList<Signal> Buy { get; private set; }
+        // -------------------------------------------------
+        #endregion //
+        // -------------------------------------------------
+
+
+        public ReversMaDeOnBar(StrategyHeader strategyHeader, 
+                               IDataContext tradingData, 
+                               ObservableQueue<Signal> signalQueue, 
+                               ILogger logger,
+                               Levels levels,
+                               IndicatorOnBarMaDeviation maDeviation)
+            //:base(tradingData.Get<ObservableCollection<Bar>>())
+        {
+            this.strategyHeader = strategyHeader;
+            this.tradingData = tradingData;
+            this.signalQueue = signalQueue;
+            this.logger = logger;
+
+            this.Levels = levels;
+            this.maDeviation = maDeviation;
+            //при появлении нового значения ValueDe вызывается обработчик OnValueItemAdded
+            this.maDeviation.AddHandlerValueDe(this.OnValueItemAdded);
+            //ипсравить - добавить OnItemAdded(Bar item) откуда вызывать вычисления индикаторов
+
+            this.Уровень = new List<int>();
+            this.УровеньПрошлый = new List<int>();
+
+            this.ПересечениeСверху = new List<bool>();
+            this.ПересечениеСнизу = new List<bool>();
+
+            ОткрытиеВерхний = new List<bool>();
+            ОткрытиеНижний = new List<bool>();
+            ЗакрытиеВерхний = new List<bool>();
+            ЗакрытиеНижний = new List<bool>();
+
+            ОткрытиеВерхнийКоличествоУровней = new List<int>();
+            ОткрытиеНижнийКоличествоУровней = new List<int>();
+            ЗакрытиеВерхнийКоличествоУровней = new List<int>();
+            ЗакрытиеНижнийКоличествоУровней = new List<int>();
+
+            //System.Collections.Generic.IList<
+            //     System.Collections.Generic.Stack<
+            //         System.Collections.Generic.IList<Signal>>> stackLevel;
+            //это создание списка стеков
+            LevelStack = new System.Collections.Generic.Stack<
+                                System.Collections.Generic.IList<Signal>
+                                                                        >[levels.КоличествоУровней];
+            //здесь создание стеков
+            for (int j = 0; j < levels.КоличествоУровней; j++)
+            {   //
+                LevelStack[j] = new System.Collections.Generic.Stack<
+                                        System.Collections.Generic.IList<Signal>>();
+            }
+            //так формируем список позиции
+            //Sell = new Signal[1];
+            //Buy = new Signal[1];
+        }
+        /*
+        public override void OnItemAdded(Bar item)
+        {
+        }*/
+    }
+}
---
+++ TRx.Handlers/ReversMaDeOnBar.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TRL.Common;
+using TRL.Common.Collections;
+using TRL.Common.Extensions.Collections;
+using TRL.Common.Data;
+using TRL.Common.Extensions.Data;
+using TRL.Common.Handlers;
+using TRL.Common.Models;
+using TRL.Logging;
+using TRL.Common.TimeHelpers;
+using TRL.Common.Events;
+using TRx.Indicators;
+using TRx.Helpers;
+
+namespace TRx.Handlers
+{
+    public partial class ReversMaDeOnBar//:AddedItemHandler<Bar>
+    {
+        /*
+        public override void OnItemAdded(Bar item)
+        {
+        }*/
+        /// <summary>
+        /// здесь проверяем пересечение отклонением уровней
+        /// </summary>
+        /// <param name="item"></param>
+        public void OnValueItemAdded(ValueDouble item)
+        {
+            //уровень текщий
+            //уровень предыдущий
+            // -------------------------------------------------
+            #region //пересечение уровней
+            // -------------------------------------------------
+            Levels.Do(maDeviation.De);
+            //IList<bool> ПересеченияСверху = levels.ПересеченияСверху;
+            //IList<bool> ПересеченияСнизу = levels.ПересеченияСнизу;
+
+            ПересечениeСверху.Add(Levels.ПересечениеСверху);
+            ПересечениеСнизу.Add(Levels.ПересечениеСнизу);
+
+            Уровень.Add(Levels.УровеньТекущий);                     //уровень текщий
+            УровеньПрошлый.Add(Levels.УровеньПрошлый);              //уровень предыдущий
+            #endregion
+            // -------------------------------------------------
+
+            ПроверитьПризнакНаОткрытиеВерхний();
+            ПроверитьПризнакНаОткрытиеНижний();
+
+            ПроверитьПризнакНаЗакрытиеВерхний();
+            ПроверитьПризнакНаЗакрытиеНижний();
+
+            // -------------------------------------------------
+            #region // Проверка сигналов по времени
+            // -------------------------------------------------
+
+            #endregion
+            // -------------------------------------------------
+
+            // -------------------------------------------------
+            #region // Подача заявок
+            // -------------------------------------------------
+            /// Сформировать Сигналы на открытие закрытие позиций
+            СформироватьСигналы();
+            #endregion
+            // -------------------------------------------------
+        }
+        /// <summary>
+        /// Сформировать Сигналы на открытие закрытие позиций
+        /// </summary>
+        private void СформироватьСигналы()
+        {
+            //throw new NotImplementedException();
+            //TODO 8. вопрос - как найти количество активных позиций
+
+            double Price = maDeviation.bar.Close;
+            int i = Уровень.Count - 1;
+            int MaxLevelPositionCount = 1;
+            int ActivePositionCount = 0;
+            ActivePositionCount = LevelStack[Уровень[i]].Count;
+            // выполнение сигналов для короткой позиции
+            if (ОткрытиеВерхний[i])
+            {
+                Signal signalSell;
+                // Если нет активной короткой позиции
+                // выдаем ордера на открыте новой короткой позиции.
+                //if (ActivePositionCount < 
