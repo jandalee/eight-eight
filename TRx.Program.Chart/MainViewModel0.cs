eee75f8eed0f27448b46e84919f1cd06f2646402
jandalee <92017819+jandalee@users.noreply.github.com>
2023-05-19 14:51:13 +0000

    Hopefully have resource display done.

---
+++ TRx.Program.Chart/MainViewModel0.cs
@@ -0,0 +1,12 @@
+﻿namespace TRx.Program.Client
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    public class MainViewModel0
+    {
+        public MainViewModel0()
+        {
+            //this.plotModel = new PlotModel { Title = "Example 1" 
